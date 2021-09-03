91aba176a57bb78391a8f2a9ddc50da74400775f
akashmadduru <akashmadduru@gmail.com>
2021-09-03 11:55:09 +0530

    adding test-cases

--- src/app/module/home/home.component.spec.ts
+++ src/app/module/home/home.component.spec.ts
@@ -1,25 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { Observable } from 'rxjs';
+import { FlickrApiService } from 'src/app/service/flickr/flickr-api.service';
+import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let service: FlickrApiService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule,
+        CommonModule,
+        HomeRoutingModule,
+        CarouselModule,
+        ButtonModule,
+        DropdownModule,
+        ProgressSpinnerModule,
+        HttpClientJsonpModule,
+        HttpClientModule,
+      ],
+      declarations: [HomeComponent],
+      providers: [FlickrApiService],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    service = fixture.debugElement.injector.get(FlickrApiService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should created', () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/service/flickr/flickr-api.service.spec.ts
+++ src/app/service/flickr/flickr-api.service.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
+import { FlickrApiService } from 'src/app/service/flickr/flickr-api.service';
 
-import { FlickrApiService } from './flickr-api.service';
-
-describe('FlickrApiService', () => {
+describe('Flickr Api Service', () => {
+  let injector: TestBed;
   let service: FlickrApiService;
+  let httpMock: HttpClientTestingModule;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FlickrApiService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [FlickrApiService],
+    });
+
+    injector = getTestBed();
+    service = injector.get(FlickrApiService);
+    httpMock = injector.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  it('Should Flickr Api Service is created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should verify URL & PARAMS', () => {
+    const URL: string = `https://api.flickr.com/services/feeds/photos_public.gne`;
+    const PARAMS: string = `?format=json&jsoncallback=JSONP_CALLBACK`;
+    expect(service.URL).toEqual(URL);
+    expect(service.PARAMS).toEqual(PARAMS);
   });
 });
